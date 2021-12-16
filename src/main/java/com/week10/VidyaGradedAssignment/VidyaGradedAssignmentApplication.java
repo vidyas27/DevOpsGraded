d06ecca18dcb969181c9aba4f96cc5c5d5517a52
vidyas27 <vidyasris2000@gmail.com>
2021-12-16 21:24:37 +0530

    Initial Commit

---
+++ src/main/java/com/week10/VidyaGradedAssignment/VidyaGradedAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.week10.VidyaGradedAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyaGradedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidyaGradedAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
